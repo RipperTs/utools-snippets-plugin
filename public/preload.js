783fa516de7ec07294bc0a063e8aa3fcc4df5c5b
ripper <wangyifani@foxmail.com>
2023-10-13 12:53:50 +0800

    替换logo

--- public/preload.js
+++ public/preload.js
@@ -1,10 +1,4 @@
-const shell = require('electron').shell
-
 const {clipboard} = require('electron')
-
-window.openExternal = function (url) {
-  shell.openExternal(url)
-}
 
 window.getClipboardContent = function () {
   return clipboard.readText('selection');
