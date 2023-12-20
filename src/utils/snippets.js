be8daabfecbc38e3a663e8d39be7a9aef3d1b8a8
ripper <wangyifani@foxmail.com>
2023-12-20 10:00:45 +0800

    fix: 可能出现的粘贴问题

--- src/utils/snippets.js
+++ src/utils/snippets.js
@@ -4,30 +4,32 @@ import _ from 'lodash';
 import placeholder_tags from "@/utils/placeholder";
 import store from '@/store'
 
-const timingMillisecond = 45 // 延迟时间
-
 // 顺序执行延迟函数
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// 程序开始执行的剪贴板内容
+let start_clipboard_content = ''
 
 /**
  * 上屏动作
  * @param code
  */
 export function snippets(code) {
+  // 获取最开始的剪贴板内容, 用于后续的还原等操作
+  start_clipboard_content = window.getClipboardContent()
+
   const snippets = window.utools.db.get(code)
   if (!snippets) {
-    window.utools.showNotification('未找到该关键字')
     window.utools.hideMainWindow()
     window.utools.outPlugin()
     return false;
   }
 
   // 手动输入内容后上屏
-  const is_input_content = checkIsInputContent(snippets.data.snippet)
-  if (is_input_content) {
+  if (checkIsInputContent(snippets.data.snippet)) {
     window.utools.setExpendHeight(0)
     window.utools.setSubInput(({text}) => {
-      // 跳转到about页面
+      // 监听手动输入的文本内容
       if (text.trim().length > 0) {
         store.state.sharedData = {text, snippets}
       } else {
@@ -37,6 +39,7 @@ export function snippets(code) {
     return true;
   }
 
+  // 其他情况直接上屏
   autoSnippets(snippets)
 }
 
@@ -49,19 +52,17 @@ export function snippets(code) {
 export function autoSnippets(snippets, input_content = '') {
   (async function () {
     store.state.sharedData = {}
-    // 获取当前剪贴板内容
-    const current_clipboard_content = window.getClipboardContent()
-    const is_select_words = checkIsSelectWords(snippets.data.snippet)
+    // 检查是否有获取划词选中内容的占位符
     let content = ''
-    if (is_select_words) {
+    if (checkIsSelectWords(snippets.data.snippet)) {
       // 需要获取划词选中内容
       window.utools.hideMainWindow()
       window.utools.simulateKeyboardTap('c', window.utools.isMacOS() ? 'command' : 'ctrl')
-      await delay(timingMillisecond);
+      await delay(100);
       let select_words = window.getClipboardContent()
-      content = processingContent(snippets.data.snippet, current_clipboard_content, select_words, input_content)
+      content = processingContent(snippets.data.snippet, start_clipboard_content, select_words, input_content)
     } else {
-      content = processingContent(snippets.data.snippet, current_clipboard_content, '', input_content)
+      content = processingContent(snippets.data.snippet, start_clipboard_content, '', input_content)
     }
     // 获取要移动到光标的位置
     let cursor_position = getCursorPosition(content)
@@ -70,19 +71,19 @@ export function autoSnippets(snippets, input_content = '') {
       content = content.replace(/{cursor}/g, '')
     }
 
-    await delay(timingMillisecond);
+    await delay(100);
     // 粘贴文本片段动作
     pasteText(snippets, content)
 
     if (cursor_position > 0) {
-      await delay(timingMillisecond);
+      await delay(80);
       for (let i = 0; i < cursor_position; i++) {
         window.utools.simulateKeyboardTap('left')
       }
     }
 
     // 执行后置动作
-    postAction(snippets, current_clipboard_content)
+    postAction(snippets, start_clipboard_content)
 
   })();
 }
@@ -116,23 +117,24 @@ function pasteText(snippets, content) {
 /**
  * 执行后置动作
  * @param snippets
- * @param current_clipboard_content
+ * @param start_clipboard_content
  */
-function postAction(snippets, current_clipboard_content) {
+function postAction(snippets, start_clipboard_content) {
   (async function () {
     const is_reduction_clipboard = snippets.data?.is_reduction_clipboard || 1
     const is_enter = snippets.data?.is_enter || 2
 
     if (is_enter === 1) {
-      await delay(timingMillisecond);
+      await delay(20);
       window.utools.simulateKeyboardTap('enter')
     }
 
     if (is_reduction_clipboard === 1) {
-      await delay(timingMillisecond);
-      window.utools.copyText(current_clipboard_content)
+      await delay(20);
+      window.utools.copyText(start_clipboard_content)
     }
 
+    await delay(50);
     window.utools.outPlugin()
 
   })();
@@ -141,27 +143,27 @@ function postAction(snippets, current_clipboard_content) {
 /**
  * 处理内容(主要替换占位符)
  * @param content
- * @param current_clipboard_content
+ * @param start_clipboard_content
  * @param select_words
  * @param input_content
  * @returns {*}
  */
-function processingContent(content, current_clipboard_content, select_words = '', input_content = '') {
+function processingContent(content, start_clipboard_content, select_words = '', input_content = '') {
   const replacements = [
     {pattern: /{datetime}/g, replacement: dayjs().format('YYYY年MM月DD日 HH:mm:ss')},
     {pattern: /{date}/g, replacement: dayjs().format('YYYY年MM月DD日')},
     {pattern: /{time}/g, replacement: dayjs().format('HH:mm:ss')},
     {pattern: /{timestamp}/g, replacement: dayjs().unix()},
     {pattern: /{isodate:(.*?)}/g, replacement: (match, format) => dayjs().format(format)},
-    {pattern: /{clipboard}/g, replacement: current_clipboard_content},
-    {pattern: /{clipboard:lowercase}/g, replacement: _.toLower(current_clipboard_content)},
-    {pattern: /{clipboard:uppercase}/g, replacement: _.toUpper(current_clipboard_content)},
-    {pattern: /{clipboard:camelcase}/g, replacement: _.camelCase(current_clipboard_content)},
-    {pattern: /{clipboard:snakecase}/g, replacement: _.snakeCase(current_clipboard_content)},
-    {pattern: /{clipboard:trim}/g, replacement: current_clipboard_content.trim()},
+    {pattern: /{clipboard}/g, replacement: start_clipboard_content},
+    {pattern: /{clipboard:lowercase}/g, replacement: _.toLower(start_clipboard_content)},
+    {pattern: /{clipboard:uppercase}/g, replacement: _.toUpper(start_clipboard_content)},
+    {pattern: /{clipboard:camelcase}/g, replacement: _.camelCase(start_clipboard_content)},
+    {pattern: /{clipboard:snakecase}/g, replacement: _.snakeCase(start_clipboard_content)},
+    {pattern: /{clipboard:trim}/g, replacement: start_clipboard_content.trim()},
     {
       pattern: /{clipboard:trim:(.*?)}/g,
-      replacement: (match, trim) => _.trim(current_clipboard_content, trim)
+      replacement: (match, trim) => _.trim(start_clipboard_content, trim)
     },
     {pattern: /{uuid}/g, replacement: uuidv4()},
     {
@@ -176,7 +178,7 @@ function processingContent(content, current_clipboard_content, select_words = ''
     {pattern: /{input:content}/g, replacement: input_content},
     {pattern: /{ip:(\d+)}/g, replacement: (match, num) => window.getIPAddress(num)},
     {pattern: /{clipboard:file:(\d+)}/g, replacement: (match, num) => getClipboardFiles(num)},
-    {pattern: /{clipboard:number}/g, replacement: () => toNumber(current_clipboard_content)},
+    {pattern: /{clipboard:number}/g, replacement: () => toNumber(start_clipboard_content)},
   ];
 
   let processedContent = content;
